ecb1dc84b3e1a35580e2512cfa0d9ff8cbcdcc95
Johannes Götz <goetz@webit.de>
2020-05-28 17:12:18 +0200

    [FEATURE] add clear button
    Refs #

---
+++ Hi.UrlRewrite/Tasks/ClearShortUrl.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Sitecore.SecurityModel;
+using Sitecore.Shell.Framework.Commands;
+
+namespace Hi.UrlRewrite.Tasks
+{
+  public class ClearShortUrl : Command
+  {
+    /// <summary>
+    /// Clears the short url field
+    /// </summary>
+    /// <param name="context">The command context</param>
+    public override void Execute(CommandContext context)
+    {
+      if (context.Items.Length != 1 || context.Items[0] == null)
+        return;
+
+      using (new SecurityDisabler())
+      {
+        context.Items[0].Editing.BeginEdit();
+        context.Items[0].Fields[Constants.ShortUrlFieldName].Value = string.Empty;
+        context.Items[0].Editing.EndEdit();
+      }
+    }
+  }
+}
