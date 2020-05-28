512bdf37de49725dbc33239dab2a734563931753
Johannes Götz <goetz@webit.de>
2020-05-28 16:56:10 +0200

    [FEATURE] add short url field type
    Refs #

---
+++ Hi.UrlRewrite/Fields/ShortUrl.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.UI;
+using Control = Sitecore.Web.UI.HtmlControls.Control;
+
+namespace Hi.UrlRewrite.Fields
+{
+  public class ShortUrl : Control
+  {
+
+    public ShortUrl()
+    {
+      Class = "scContentControl";
+    }
+    
+    protected override void DoRender(HtmlTextWriter output)
+    {
+      string err = null;
+      output.Write("<input" + GetControlAttributes() + "value='" + Value + "' readonly >");
+      
+    }
+  }
+}
