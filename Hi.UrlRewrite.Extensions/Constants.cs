aab98551d47ce32a067e533dba39230d74a5faae
Johannes Götz <goetz@webit.de>
2020-07-10 16:53:39 +0200

    [FEATURE] handle de-activation of commands
    Refs #

---
+++ Hi.UrlRewrite.Extensions/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Sitecore.Data;
+
+namespace Hi.UrlRewrite.Extensions
+{
+  public class Constants
+  {
+    public static readonly ID RedirectExportDialogueScript =
+      ID.Parse(Guid.Parse("{5A3D2D3B-C56F-4AEF-84E8-08FCB9598BFE}"));
+
+    public static readonly ID RedirectImportDialogueScript =
+      ID.Parse(Guid.Parse("{5A3D2D3B-C56F-4AEF-84E8-08FCB9598BFE}"));
+  }
+}
--- Hi.UrlRewrite.Extensions/Tasks/ExportRedirects.cs
+++ Hi.UrlRewrite.Extensions/Tasks/ExportRedirects.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using Sitecore.Diagnostics;
 using Sitecore.Shell.Framework.Commands;
+using Sitecore.StringExtensions;
+using Spe.Core.Host;
 
 namespace Hi.UrlRewrite.Extensions.Tasks
 {
@@ -11,7 +14,49 @@ namespace Hi.UrlRewrite.Extensions.Tasks
     /// <param name="context">The command context</param>
     public override void Execute(CommandContext context)
     {
-      throw new NotImplementedException();
+      using (ScriptSession session = ScriptSessionManager.NewSession("Defailt", false))
+      {
+        var scriptitem = Sitecore.Context.Database.GetItem(Constants.RedirectExportDialogueScript);
+        var script = scriptitem["Script"];
+        if (script.IsNullOrEmpty())
+        {
+          return;
+        }
+
+        try
+        {
+          session.ExecuteScriptPart(script);
+        }
+        catch (Exception e)
+        {
+          Console.WriteLine("There has been an error executing the redirect export script: \n" + e.Message);
+          throw;
+        }
+      }
+    }
+
+    /// <summary>
+    /// Check if the command is active
+    /// </summary>
+    /// <param name="context">The command context</param>
+    /// <returns>CommandState.Enabled if the selected item is a redirect folder or redirect subfolder item. CommandState.Disabled otherwise.</returns>
+    public override CommandState QueryState(CommandContext context)
+    {
+      Assert.ArgumentNotNull((object)context, nameof(context));
+
+      if (context.Items.Length != 1 || context.Items[0] == null)
+      {
+        return CommandState.Disabled;
+      }
+
+      var itemTemplate = context.Items[0].TemplateID.ToString();
+      if (itemTemplate.Equals(Templates.Folders.RedirectFolderItem.TemplateId)
+      || itemTemplate.Equals(Templates.Folders.RedirectSubFolderItem.TemplateId))
+      {
+        return CommandState.Enabled;
+      }
+
+      return CommandState.Disabled;
     }
   }
 }
