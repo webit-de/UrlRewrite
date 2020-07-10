03f7dc0f0d164f6e3b3060af0c76edad017be450
Johannes Götz <goetz@webit.de>
2020-07-10 12:24:33 +0200

    [FEATURE] add empty commands
    Refs #

---
+++ Hi.UrlRewrite.Extensions/Tasks/ExportRedirects.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Sitecore.Shell.Framework.Commands;
+
+namespace Hi.UrlRewrite.Extensions.Tasks
+{
+  public class ExportRedirects : Command
+  {
+    /// <summary>
+    /// Starts the export redirects dialogue
+    /// </summary>
+    /// <param name="context">The command context</param>
+    public override void Execute(CommandContext context)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
---
+++ Hi.UrlRewrite.Extensions/Tasks/ImportRedirects.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Sitecore.Shell.Framework.Commands;
+
+namespace Hi.UrlRewrite.Extensions.Tasks
+{
+  public class ImportRedirects : Command
+  {
+    /// <summary>
+    /// Starts the import redirects dialogue
+    /// </summary>
+    /// <param name="context">The command context</param>
+    public override void Execute(CommandContext context)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
