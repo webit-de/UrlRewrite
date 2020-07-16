0d1a1a47585105f371db397eda0e06e155dcc1ab
Johannes Götz <goetz@webit.de>
2020-07-16 18:33:25 +0200

    [BUGFIX] return after deleting redirect
    Refs #

--- Hi.UrlRewrite.Extensions/Services/RedirectImportService.cs
+++ Hi.UrlRewrite.Extensions/Services/RedirectImportService.cs
@@ -57,6 +57,7 @@ namespace Hi.UrlRewrite.Extensions.Services
       if (redirect.Status.ToUpper() == Constants.ImportStatus.DELETE.ToString())
       {
         DeleteRedirect(redirect);
+        return;
       }
 
       // update redirect or create a new one
