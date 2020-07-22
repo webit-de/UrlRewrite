484253ebcd72b200fbc61c7ef2fa5720196d5bc4
Johannes Götz <goetz@webit.de>
2020-07-22 10:39:01 +0200

    [TASK] clean up import controller
    Refs #

--- Hi.UrlRewrite.Extensions/Controllers/ImportRedirectsController.cs
+++ Hi.UrlRewrite.Extensions/Controllers/ImportRedirectsController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Web.Mvc;
 using Hi.UrlRewrite.Extensions.Services;
 using Sitecore.Data;
@@ -11,12 +12,18 @@ namespace Hi.UrlRewrite.Extensions.Controllers
   [Authorize]
   public class ImportRedirectsController : SitecoreController
   {
+    /// <summary>
+    /// Import redirects from a csv media item
+    /// </summary>
+    /// <param name="csvItemId">The item id</param>
+    /// <param name="rootFolderId">The id of the root folder to import into.</param>
+    /// <returns>The warning log as csv.</returns>
     public ActionResult ImportRedirects(string csvItemId, string rootFolderId)
     {
       Assert.IsNotNullOrEmpty(csvItemId, "CSV item Id must not be empty");
       Assert.IsNotNullOrEmpty(rootFolderId, "Root folder item Id must not be empty");
 
-      // since this route is only mapped for CM instances, we can use the master database.
+      // since this route is only mapped for CM instances, the master database can be used.
       var db = Sitecore.Configuration.Factory.GetDatabase("master");
       var rootFolder = db.GetItem(ID.Parse(Guid.Parse(rootFolderId)));
 
@@ -25,17 +32,23 @@ namespace Hi.UrlRewrite.Extensions.Controllers
         return Content("Please select a Redirect Root folder for importing.");
       }
 
-      var csvItem = db.GetItem(ID.Parse(Guid.Parse(csvItemId)));
-      var csvMediaItem = new MediaItem(csvItem);
-
-      var stream = csvMediaItem.GetMediaStream();
+      var csvStream = CsvService.GetCsvStream(db, csvItemId);
+      if (csvStream == null)
+      {
+        return Content("Could not create a csv stream from the provided item.");
+      }
 
       var importService = new RedirectImportService(db);
-      var warningLog = importService.GenerateRedirectsFromCsv(stream, rootFolder);
+      var warningLog = importService.GenerateRedirectsFromCsv(csvStream, rootFolder);
       
       return Content(warningLog);
     }
 
+    /// <summary>
+    /// Check if the root folder id points to a redirect folder item
+    /// </summary>
+    /// <param name="rootFolder">The root folder id</param>
+    /// <returns>True, if the id points to a RedirectFolderItem</returns>
     private static bool IsValidRootFolder(Item rootFolder)
     {
       return rootFolder != null && rootFolder.TemplateID.ToString() == Templates.Folders.RedirectFolderItem.TemplateId;
--- Hi.UrlRewrite.Extensions/Services/CsvService.cs
+++ Hi.UrlRewrite.Extensions/Services/CsvService.cs
@@ -1,14 +1,23 @@
 ﻿
+using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 using CsvHelper;
 using Hi.UrlRewrite.Extensions.Models;
+using Sitecore.Data;
+using Sitecore.Data.Items;
 
 namespace Hi.UrlRewrite.Extensions.Services
 {
-  public class CsvService
+  public static class CsvService
   {
+    /// <summary>
+    /// Generate a CSV from an enumerable of <see cref="ICsvModel"/>.
+    /// </summary>
+    /// <typeparam name="T">The model Type</typeparam>
+    /// <param name="entries">The entries.</param>
+    /// <returns>CSV as byte[]</returns>
     public static byte[] GenerateCsv<T>(IEnumerable<T> entries) where T : ICsvModel
     {
       var csvStream = new MemoryStream();
@@ -19,6 +28,26 @@ namespace Hi.UrlRewrite.Extensions.Services
         writer.Flush();
         return csvStream.ToArray();
       }
+    }
+
+    /// <summary>
+    /// Get the csv stream from a CSV Media file
+    /// </summary>
+    /// <param name="db"></param>
+    /// <param name="csvItemId"></param>
+    /// <returns></returns>
+    public static Stream GetCsvStream(Database db, string csvItemId)
+    {
+      var csvItem = db.GetItem(ID.Parse(Guid.Parse(csvItemId)));
+
+      if (csvItem == null)
+      {
+        return null;
+      }
+
+      var csvMediaItem = new MediaItem(csvItem);
+
+      return csvMediaItem.Extension.ToLower() != "csv" ? null : csvMediaItem.GetMediaStream();
     }
   }
 }
