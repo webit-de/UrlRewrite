5d444acfa70006072ab9527a19365869c077075d
Martin Schrader <schrader@webit.de>
2020-07-30 08:55:04 +0200

    [FEATURE] Introduce new StringExtension class
    * add `HasValue` method for strings

---
+++ Hi.UrlRewrite/Extensions/StringExtension.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Sitecore.Shell.Framework.Commands.System;
+using Sitecore.StringExtensions;
+
+namespace Hi.UrlRewrite.Extensions
+{
+  /// <summary>
+  /// String extension class
+  /// </summary>
+  public static class StringExtension
+  {
+    /// <summary>
+    /// Indicates whether the specified string has a value, i.e. Not null or empty
+    /// </summary>
+    /// <param name="target">The string to extended.</param>
+    /// <returns>Returns <code>true</code> if the <see cref="target"/> string has as value; otherwise <code>false</code>.</returns>
+    public static bool HasValue(this string target)
+    {
+      return !target.IsNullOrEmpty();
+    }
+  }
+}
