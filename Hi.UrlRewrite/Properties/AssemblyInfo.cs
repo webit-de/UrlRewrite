5dc377df6c8a011b783f687c59db4cf0b3b2e7ff
Izbéki Ádám <izbeki.adam@allwin.hu>
2023-03-24 09:04:06 +0100

    Update version, Sitecore install package, solution package reference

--- Hi.UrlRewrite/Properties/AssemblyInfo.cs
+++ Hi.UrlRewrite/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyTitle("Hi.UrlRewrite")]
 [assembly: AssemblyDescription("Url Rewrite for Sitecore")]
-[assembly: AssemblyInformationalVersion("v1.9.1")]
+[assembly: AssemblyInformationalVersion("v1.9.2")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Url Rewrite")]
@@ -31,4 +31,4 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.9.1")]
+[assembly: AssemblyVersion("1.9.2")]
